0a4187628536490b5874300b9238d629cf0a7b1c
cFaley <cfaley5097@stu.d214.org>
2025-04-14 10:18:45 -0500

    Slots start

--- ContentView.swift
+++ ContentView.swift
@@ -5,6 +5,21 @@ struct ContentView: View {
         VStack {
             
             Text("I Love Gambling❤️")
+            
+            Button {
+                
+            } label: {
+                ZStack{
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .frame(width: 300, height: 100, alignment: .center)
+                    Text("Slots")
+                        .font(.largeTitle)
+                        .foregroundStyle(.black)
+                        
+                }
+            }
+            
         }
     }
 }

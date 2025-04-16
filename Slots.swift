af87e602e7cb7c8a24347d893e2ddbe01699e545
cFaley <cfaley5097@stu.d214.org>
2025-04-16 11:03:33 -0500

    SLOTS DONE

--- Slots.swift
+++ Slots.swift
@@ -6,9 +6,10 @@ struct Slots: View {
     @State var slot3 = 0
 //    @State var IsSpinning = true
 //    @State var count = 0
+    @State var cash = 5000
     var body: some View {
         VStack {
-            
+            Text("Cash $\(cash)")
             HStack{
                 ZStack{
                     Rectangle()
@@ -16,6 +17,7 @@ struct Slots: View {
 //                    Text("\(slot1)")
                     Image("Image\(slot1)")
                         .resizable()
+                        .frame(width: 100, height: 100)
                     
                 }
                 .frame(width: 100, height: 200, alignment: .leading)
@@ -25,6 +27,7 @@ struct Slots: View {
 //                    Text("\(slot2)")
                     Image("Image\(slot2)")
                         .resizable()
+                        .frame(width: 100, height: 100)
                 }
                 .frame(width: 100, height: 200, alignment: .center)
                 ZStack{
@@ -33,6 +36,7 @@ struct Slots: View {
 //                    Text("\(slot3)")
                     Image("Image\(slot3)")
                         .resizable()
+                        .frame(width: 100, height: 100)
                 }
                 .frame(width: 100, height: 200, alignment: .trailing)
             }
@@ -40,6 +44,7 @@ struct Slots: View {
                 //                slot1 = Int.random(in: 1..<4)
                 //                slot2 = Int.random(in: 1..<4)
                 //                slot3 = Int.random(in: 1..<4)
+                cash -= 100
                 Spin()
                 //                                IsSpinning = false
                 
@@ -52,10 +57,45 @@ struct Slots: View {
                         .foregroundStyle(.black)
                 }
             }
-            Text("$100")
+            Text("$100 Per Spin")
+                .font(.largeTitle)
+            Image("Chart1")
+                .resizable()
+                .frame(width: 300, height: 200, alignment: .center)
         }
     }
 
+    func AddMoney() {
+        if slot1 == 2{
+            if slot2 == 2{
+                if slot3 == 2{
+                    cash += 9000
+                }
+            }
+        }
+        if slot1 == slot2{
+            if slot2 == slot3{
+                cash += 1000
+            }
+            else{
+                cash += 200
+            }
+        }
+        else{
+            if slot2 == slot3{
+                cash += 200
+            }
+            else{
+                if slot1 == slot3{
+                    cash += 200
+                    
+                }
+            }
+                
+        }
+    }
+    
+    
     func Spin() {
         Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
             let randomNumber = Int.random(in: 1...20)
@@ -68,10 +108,13 @@ struct Slots: View {
             }
 
             if randomNumber == 10 {
+                DispatchQueue.main.async {
+                    self.AddMoney()
+                }
                 timer.invalidate()
             }
         }
     }
-
+    
     
 }
